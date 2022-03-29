3dc8ed18196acbef76479519003302a7f7481601
Abhishek jaiswal <abhicse003@gmail.com>
2022-03-30 01:35:21 +0530

    Update lab2ping.cc

--- lab2ping.cc
+++ lab2ping.cc
@@ -36,21 +36,27 @@ main (int argc, char *argv[])
   cmd.Parse (argc, argv);
   // Here, we will explicitly create six nodes.
   NodeContainer c;
+  // NS_LOG_INFO ("Create nodes.");
   c.Create (6);
   // connect all our nodes to a shared channel.
+  // NS_LOG_INFO ("Build Topology.");
   CsmaHelper csma;
   csma.SetChannelAttribute ("DataRate", DataRateValue (DataRate (10000)));
   csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (0.2)));
   NetDeviceContainer devs = csma.Install (c);
   // add an ip stack to all nodes.
+  // NS_LOG_INFO ("Add ip stack.");
   InternetStackHelper ipStack;
   ipStack.Install (c);
-  
   // assign ip addresses
+  // NS_LOG_INFO ("Assign ip addresses.");
   Ipv4AddressHelper ip;
   ip.SetBase ("192.168.1.0", "255.255.255.0");
   Ipv4InterfaceContainer addresses = ip.Assign (devs);
+  // NS_LOG_INFO ("Create Sink.");
+  // NS_LOG_INFO ("Create Applications.");
   uint16_t port = 9;   // Discard port (RFC 863)
+ // OnOffHelper onoff ("ns3::UdpSocketFactory", 
   OnOffHelper onoff ("ns3::UdpSocketFactory",Address (InetSocketAddress (addresses.GetAddress (2), port)));
   onoff.SetConstantRate (DataRate ("500Mb/s"));
   ApplicationContainer app = onoff.Install (c.Get (0));
@@ -61,6 +67,7 @@ main (int argc, char *argv[])
   PacketSinkHelper sink ("ns3::UdpSocketFactory",Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
   app = sink.Install (c.Get (2));
   app.Start (Seconds (0.0));
+  // NS_LOG_INFO ("Create pinger");
   V4PingHelper ping = V4PingHelper (addresses.GetAddress (2));
   NodeContainer pingers;
   pingers.Add (c.Get (0));
@@ -69,11 +76,12 @@ main (int argc, char *argv[])
   apps = ping.Install (pingers);
   apps.Start (Seconds (1.0));
   apps.Stop (Seconds (5.0));
-
   // finally, print the ping rtts.
   Config::Connect ("/NodeList/*/ApplicationList/*/$ns3::V4Ping/Rtt",MakeCallback (&PingRtt));
+  //  NS_LOG_INFO ("Run Simulation.");
   AsciiTraceHelper ascii;
   csma.EnableAsciiAll (ascii.CreateFileStream ("ping1.tr"));
   Simulator::Run ();
   Simulator::Destroy ();
+  // NS_LOG_INFO ("Done.");
 }
